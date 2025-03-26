15522a2f9be157ab1a8e63653a7b2663472d3caf
Gustavo Bongiovani <132799669+bongiovanii@users.noreply.github.com>
2025-03-25 22:21:30 -0300

    tem que arrumar

--- Cavaleiros_Do_Apocalipse/src/controller/ThreadCavaleiro.java
+++ Cavaleiros_Do_Apocalipse/src/controller/ThreadCavaleiro.java
@@ -3,98 +3,114 @@ package controller;
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 
-public class ThreadCavaleiro extends Thread{
-	private static final Semaphore semaforo = new Semaphore(1);
+public class ThreadCavaleiro extends Thread {
+	private static final Semaphore semaforo = new Semaphore(4);
 	private static final Semaphore semaforoTocha = new Semaphore(1);
-	private static final Semaphore semaforoPedra= new Semaphore(1);
+	private static final Semaphore semaforoPedra = new Semaphore(1);
 
 	private final String cavaleiro;
+
+	private static boolean isTocha = false;
+	private static boolean isPedra = false;
 	
-	private boolean isTocha = false;
-	private boolean isPedra = false;
-	
+
 	public ThreadCavaleiro(String cavaleiro, boolean isTocha, boolean isPedra) {
 		this.cavaleiro = cavaleiro;
 		this.isTocha = isTocha;
 		this.isPedra = isPedra;
 	}
-	
+
 	public void run() {
 		try {
-			semaforo.acquire();		
+			semaforo.acquire();
 			cavaleiroAndou();
-			Thread.sleep(50);
 			
-		}catch(InterruptedException e) {
+
+		} catch (InterruptedException e) {
 			e.fillInStackTrace();
 		}
-			
-	}	
-	
 
+	}
 
 	private void cavaleiroAndou() {
 		Random rand = new Random();
 		int min = 2;
 		int max = 4;
-		int deslocamento = rand.nextInt((max - min) + 1) + min;;
-		
+		int deslocamento = rand.nextInt((max - min) + 1) + min;
+
 		int distanciaCorredor = 2000;
 		int distanciaPercorrida = 0;
+
 		
-		while(distanciaPercorrida < distanciaCorredor) {
+
+		while (distanciaPercorrida < distanciaCorredor) {
 			distanciaPercorrida += deslocamento;
-			System.out.println("O cavaleiro "+ cavaleiro + " andou " + distanciaPercorrida + "m.");
-			if(distanciaPercorrida == 500) {
-				try {
-					semaforoTocha.acquire();
-					System.out.println("O cavaleiro "+ cavaleiro + " pegou a tocha");
-					isTocha = true;
-					
-				}catch (Exception e) {
-					e.fillInStackTrace();
-				}finally {
-					semaforoTocha.release();
-					
-				}
-				
-				if(isTocha == false) {
+			System.out.println("O cavaleiro " + cavaleiro + " andou " + distanciaPercorrida + "m.");
+			
+			if (distanciaPercorrida == 500) {
+				boolean pegou = pegarTocha(isTocha);
+				if(pegou == false) {
 					try {
+						System.out.println("Cavaleiro " + cavaleiro + ". Sinto muito, alguém ja pegou a tocha...");
 						Thread.sleep(50);
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					}
+					} 
 				}else {
 					deslocamento += 2;
 				}
 			}
 			
-			if(distanciaPercorrida ==  1500) {
-				if(isTocha == true) {
-					System.out.println("O cavaleiro "+ cavaleiro + " já pegou a tocha, por isso não pegará a pedra");
-					isPedra = false;
-				}else if(isTocha == false) {
-					try {
-						semaforoPedra.acquire();
-						System.out.println("O cavaleiro "+ cavaleiro + " pegou a pedra");
-						isPedra = true;
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}finally {
-						semaforoPedra.release();
-					}
-					
-				
-				}	
+
+			if (distanciaPercorrida == 1500) {
+				boolean pegouPedra = pegarPedra(isPedra);
 			}
+			
+			if(distanciaPercorrida == 1500 && isPedra == true) {
+				try {
+					System.out.println(cavaleiro + " você ja pegou a pedra...");
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+	
 		}
-		
-		
-		
-		
-		
+
+	}
+
+	private boolean pegarPedra(boolean isPedra) {
+		if (isTocha == false) {
+			try {
+				semaforoPedra.acquire();
+				System.out.println("O cavaleiro " + cavaleiro + " pegou a pedra...");
+				isPedra = true;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} 
+
+		}
+		return isPedra;
+	}
+
+	private boolean pegarTocha(boolean isTocha) {
+		if(isTocha == false) {
+			try {
+				semaforoTocha.acquire();
+				System.out.println("O cavaleiro " + cavaleiro + " pegou a tocha...");
+				isTocha = true;
+				
+
+			} catch (Exception e) {
+				e.fillInStackTrace();
+			}finally {
+				semaforoTocha.release();
+			}
+
+		}
+		return isTocha;
 	}
 
 }
--- Cavaleiros_Do_Apocalipse/src/view/Corredor.java
+++ Cavaleiros_Do_Apocalipse/src/view/Corredor.java
@@ -3,15 +3,15 @@ package view;
 import controller.ThreadCavaleiro;
 
 public class Corredor {
-	public static void main(String[]args) {
-		String[] cavaleiros = {"Conquista","Guerra","Fome","Morte" };
-		
-		for(String cavaleiro : cavaleiros) {
+	public static void main(String[] args) {
+		String[] cavaleiros = { "Conquista", "Guerra", "Fome", "Morte" };
+
+		for (String cavaleiro : cavaleiros) {
 			boolean isPedra = false;
 			boolean isTocha = false;
 			new ThreadCavaleiro(cavaleiro, isTocha, isPedra).start();
 		}
-		
+
 	}
 
 }
